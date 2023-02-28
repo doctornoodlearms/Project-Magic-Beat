e15c4a9a9fc2e3951c6d606392c3b7e1ea86a289
Doctornoodlearms <doctornoodlearms11@gmail.com>
2023-02-27 22:26:29 -0500

    Added inputs for all beatmap keys and works

--- Scripts/BeatMap.gd
+++ Scripts/BeatMap.gd
@@ -8,5 +8,16 @@ func _ready():
 
 func addNote():
 	
-	var note:Note = noteScene.instance()
-	$Path1.addNote(note)
+	var note1:Note = noteScene.instance()
+	var note2:Note = noteScene.instance()
+	var note3:Note = noteScene.instance()
+	var note4:Note = noteScene.instance()
+	var note5:Note = noteScene.instance()
+	var note6:Note = noteScene.instance()
+	
+	$Path1.addNote(note1)
+	$Path2.addNote(note2)
+	$Path3.addNote(note3)
+	$Path4.addNote(note4)
+	$Path5.addNote(note5)
+	$Path6.addNote(note6)
--- Scripts/BeatPath.gd
+++ Scripts/BeatPath.gd
@@ -1,5 +1,7 @@
 extends Path2D
 
+export var keybind:int = 1
+
 var noteList:Array = []
 
 func addNote(newNote:Note):
@@ -9,13 +11,16 @@ func addNote(newNote:Note):
 	newNote.connect("RemovingNote", self, "_removeNote")
 	add_child(newNote)
 	
+func _ready():
+	pass
+
 func _removeNote():
 	
 	noteList.pop_front()
 
 func _input(event: InputEvent) -> void:
 	
-	if(Input.is_action_just_pressed("beatmap_input1")):
+	if(Input.is_action_just_pressed("beatmap_input" + keybind as String)):
 		
 		if(noteList.size() <= 0):
 			return
